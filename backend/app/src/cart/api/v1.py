e1c23c7427f38b986cd1b7ce0d6b93d1ed970b34
= <=>
2022-08-29 17:23:46 +0100

    completed cart route

--- backend/app/src/cart/api/v1.py
+++ backend/app/src/cart/api/v1.py
@@ -7,19 +7,27 @@ from app.src.cart import crud, schemas
 
 cart = APIRouter()
 
+
 @cart.post("/", response_model=Message, status_code=status.HTTP_201_CREATED)
-async def create_cart(cart:schemas.CartIn, user:User = Depends(UserWrite.current_user_with_data)):
+async def create_cart(cart: schemas.CartIn, user: User = Depends(UserWrite.current_user_with_data)):
     return await crud.create_cart(cart, user)
 
 
-@cart.put("/{id}", response_model=Message, status_code=status.HTTP_200_OK)
-async def update_cart(id:int, cart:schemas.CartIn, user:User = Depends(UserWrite.current_user_with_data)):
-    return await crud.update_cart(id, cart, user)
+@cart.put("/", response_model=schemas.CartOut, status_code=status.HTTP_200_OK)
+async def update_cart(cart: schemas.CartUpdateIn, user: User = Depends(UserWrite.current_user_with_data)):
+    return await crud.update_cart(cart, user)
 
-@cart.get("/", )
-async def get_carts(user:User = Depends(UserWrite.current_user_with_data)):
+
+@cart.get("/", response_model=List[schemas.CartOut])
+async def get_carts(user: User = Depends(UserWrite.current_user_with_data)):
     return await crud.get_all_cart(user)
 
-@cart.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_cart(id:int, user:User = Depends(UserWrite.current_user_with_data)):
-    return await crud.delete_cart(id, user)
+
+@cart.get("/{cartId}", response_model=schemas.CartOut, status_code=status.HTTP_200_OK)
+async def get_cart(cartId: int, user: User = Depends(UserWrite.current_user_with_data))->schemas.CartOut:
+    return await crud.get_cart(cartId, user)
+
+
+@cart.delete("/{cartId}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_cart(cartId: int, user: User = Depends(UserWrite.current_user_with_data)):
+    return await crud.delete_cart(cartId, user)
--- backend/app/src/cart/crud.py
+++ backend/app/src/cart/crud.py
@@ -1,55 +1,66 @@
-from fastapi import  status, HTTPException, Response
+from fastapi import status, HTTPException, Response
 from app.src._base.schemas import Message
-from app.src.cart import  schemas
+from app.src.cart import schemas
 from app.src.cart.models import Cart
 from app.src.user.models import User
 from app.src.product.models import Product, ProductProperty
 
 
-async def check_product_in_quantity(productId:int, cart:schemas.CartIn) -> Cart:
-    get_product:Product = await Product.objects.select_related(['property']).get_or_none(id=productId)
+async def check_product_in_quantity(productId: int, cart: schemas.CartIn) -> Cart:
+    get_product: Product = await Product.objects.select_related(['property']).get_or_none(id=productId)
     if not get_product:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='book does not exist')
-    product_prop:ProductProperty = get_product.property
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='book does not exist')
+    product_prop: ProductProperty = get_product.property
     if not product_prop.paper_back_qty >= cart.paper_back_qty:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Hard back book quantity is less than cart quantity, `{product_prop.paper_back_qty}`')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Hard back book quantity is less than cart quantity, `{product_prop.paper_back_qty}`')
     if not product_prop.hard_back_qty >= cart.hard_back_qty:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Hard back book quantity is less than cart quantity, `{product_prop.hard_back_qty}`')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Hard back book quantity is less than cart quantity, `{product_prop.hard_back_qty}`')
     return get_product
 
 
-async def create_cart(cart:schemas.CartIn, user:User ):
+async def create_cart(cart: schemas.CartIn, user: User):
     get_product = await check_product_in_quantity(cart.productId, cart)
     add_cart = await Cart.objects.create(
-        pdf = cart.pdf,
-        paper_back_qty = cart.paper_back_qty,
-        hard_back_qty = cart.hard_back_qty,
-        user=user, 
+        pdf=cart.pdf,
+        paper_back_qty=cart.paper_back_qty,
+        hard_back_qty=cart.hard_back_qty,
+        user=user,
         product=get_product)
-    if  add_cart:
-       return Message(message='Cart created successfully')
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='product does not exist in cart')
-    
+    if add_cart:
+        return Message(message='Cart created successfully')
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail='product does not exist in cart')
 
 
-async def update_cart(id:int, cart:schemas.CartIn, user:User):
-    await check_product_in_quantity(cart.productId, cart)
-    get_cart_product = await Cart.objects.filter(id=id, user=user).first()
+async def update_cart(cart: schemas.CartUpdateIn, user: User):
+    get_cart_product = await Cart.objects.select_related(["product__property", "product__cover_img"]).filter(id=cart.cartId, user=user).first()
     if not get_cart_product:
-        raise  HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='product does not exist in cart')
-    for key, value in cart.dict(exclude=['productId']).items():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='product does not exist in cart')
+    await check_product_in_quantity(get_cart_product.product.id, cart)
+    for key, value in cart.dict(exclude={'cartId'}).items():
         setattr(get_cart_product, key, value)
-    await get_cart_product.save()
+    await get_cart_product.upsert()
     return get_cart_product
 
-async def get_all_cart(user:User):
-    data = await Cart.objects.filter(user=user).select_related(['product']).all()
-    return data
-    
 
-async def delete_cart(id:int, user:User):
-    get_cart = await Cart.objects.get_or_none(id=id, user=user)
+async def get_all_cart(user: User):
+    data = await Cart.objects.select_related(["product__property", "product__cover_img"]).filter(user=user).all()
+    return data
+
+
+async def get_cart(cartId: int, user: User):
+    data = await Cart.objects.select_related(["product__property", "product__cover_img"]).filter(id=cartId, user=user).first()
+    return data
+
+
+async def delete_cart(cartId: int, user: User):
+    get_cart = await Cart.objects.get_or_none(id=cartId, user=user)
     if not get_cart:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='product does not exist in cart')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='product does not exist in cart')
     await get_cart.delete()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
